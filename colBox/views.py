103f3df17b0b1ae44a17e02af25a4d84e472af2a
Victor Ondari <vic@Victors-MacBook-Pro.local>
2022-05-27 00:36:05 +0300

    added the function to display pthotos.

--- colBox/views.py
+++ colBox/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
+from .models import Image, Location, Category
+import datetime as dt
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    # images=Image.today_images()
+    images =Image.get_latest()
+    return render(request, 'index.html',{'images':images})
+
+def show(request):
+    images = Image.get_images()
+    return render(request, 'show.html', {'images': images})
