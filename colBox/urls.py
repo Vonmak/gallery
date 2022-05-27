337b7b0b03b3f02c212a65532e95311db8526f84
Victor Ondari <vic@Victors-MacBook-Pro.local>
2022-05-27 13:13:10 +0300

    added a url for showing locations data.

--- colBox/urls.py
+++ colBox/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path, re_path
 from . import views
 from django.conf import settings
 from django.conf.urls.static import static
@@ -8,6 +8,7 @@ urlpatterns = [
     path('',views.index, name='index'),
     path('show', views.show, name='show'),
     path('search/', views.search_results, name='searchResults'),
+    re_path('^location/(?P<locale>\w+)/', views.location, name='location'),
 ]
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
