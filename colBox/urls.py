3900778d71d3e0c312caea07f04d00f796f5cc55
Victor Ondari <vic@Victors-MacBook-Pro.local>
2022-05-27 11:36:18 +0300

    added a path followed when search is inciated.

--- colBox/urls.py
+++ colBox/urls.py
@@ -6,7 +6,8 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('',views.index, name='index'),
-    path('show', views.show, name='show')
+    path('show', views.show, name='show'),
+    path('search/', views.search_results, name='searchResults'),
 ]
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
