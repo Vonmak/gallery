8554fbdc7b3f0b67edd09ae77e5dbeda78c33d45
Victor Ondari <vic@Victors-MacBook-Pro.local>
2022-05-31 06:30:58 +0300

    statics updates.

--- colBox/urls.py
+++ colBox/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path, re_path
 from . import views
-from django.conf import settings
-from django.conf.urls.static import static
+# from django.conf import settings
+# from django.conf.urls.static import static
 
 
 urlpatterns = [
@@ -11,5 +11,5 @@ urlpatterns = [
     path('search/', views.search_results, name='searchResults'),
     re_path('^location/(?P<locale>\w+)/', views.location, name='location'),
 ]
-if settings.DEBUG:
-    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+# if settings.DEBUG:
+#     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
--- gallery/settings.py
+++ gallery/settings.py
@@ -131,14 +131,13 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 
 STATIC_URL = 'static/'
 
 # Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
+STATIC_ROOT=os.path.join(BASE_DIR,"/static/") 
 
 # STATIC_ROOT = '/Users/vic/Documents/django/theGallery/static',
 
@@ -160,5 +159,4 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-# Configure Django App for Heroku.
-# django_heroku.settings(locals())
+
